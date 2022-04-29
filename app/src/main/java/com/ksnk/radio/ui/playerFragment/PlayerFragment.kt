27ded0792f4876a9f34e269b62462b8e16a665d1
Dmitry Kosenko <71468576+Ksnk-dm@users.noreply.github.com>
2022-04-29 16:45:47 +0300

    update

---
+++ app/src/main/java/com/ksnk/radio/ui/playerFragment/PlayerFragment.kt
@@ -0,0 +1,34 @@
+package com.ksnk.radio.ui.playerFragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ksnk.radio.R
+import dagger.android.support.AndroidSupportInjection
+
+class PlayerFragment : Fragment() {
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.second_fragment, container, false);
+    }
+
+
+
+    companion object
+
+    fun newInstance(): PlayerFragment {
+        return PlayerFragment()
+    }
+}
