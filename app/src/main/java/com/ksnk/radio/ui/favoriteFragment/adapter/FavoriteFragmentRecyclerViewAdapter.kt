4119c0bbf37e15394691079a93d16bd2a18b4127
Dmitry Kosenko <71468576+Ksnk-dm@users.noreply.github.com>
2022-05-23 23:32:01 +0300

    ref

--- app/src/main/java/com/ksnk/radio/ui/favoriteFragment/adapter/FavoriteFragmentRecyclerViewAdapter.kt
+++ app/src/main/java/com/ksnk/radio/ui/favoriteFragment/adapter/FavoriteFragmentRecyclerViewAdapter.kt
@@ -31,11 +31,15 @@ class FavoriteFragmentRecyclerViewAdapter(
             .load(items[position].image)
             .into(holder.imageViewWave)
         holder.itemView.setOnClickListener {
-            val mediaItem: MediaItem = MediaItem.fromUri(items[position].url.toString())
-            mPlayer.setMediaItem(mediaItem)
-            mPlayer.play()
-            mService?.setRadioWave(items[position])
+            setRadioWaveAndPlay(position)
         }
+    }
+
+    private fun setRadioWaveAndPlay(position: Int) {
+        val mediaItem: MediaItem = MediaItem.fromUri(items[position].url.toString())
+        mPlayer.setMediaItem(mediaItem)
+        mPlayer.play()
+        mService.setRadioWave(items[position])
     }
 
     override fun getItemCount(): Int {
